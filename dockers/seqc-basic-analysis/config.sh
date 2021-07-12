be210558797303fb86294b42e4620c040ddc49c2
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-07-12 07:39:58 -0400

    Set version to 0.0.7

--- dockers/seqc-basic-analysis/config.sh
+++ dockers/seqc-basic-analysis/config.sh
@@ -1,7 +1,8 @@
-version="0.0.6"
+version="0.0.7"
 
 # docker related
 registry="583643567512.dkr.ecr.us-east-1.amazonaws.com"
+registry="hisplan"
 image_name="seqc-basic-analysis"
 
 # set this to 1 if you want to create a repository in AWS ECR
