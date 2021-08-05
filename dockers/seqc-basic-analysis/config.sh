d0686d08ea89ad303462dcfe7aab7919a4372e4f
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-08-05 06:48:22 -0400

    Refactor for SCING

--- dockers/seqc-basic-analysis/config.sh
+++ dockers/seqc-basic-analysis/config.sh
@@ -1,10 +1,5 @@
-version="0.0.8"
+version="0.0.9"
 
 # docker related
-registry="583643567512.dkr.ecr.us-east-1.amazonaws.com"
 registry="hisplan"
 image_name="seqc-basic-analysis"
-
-# set this to 1 if you want to create a repository in AWS ECR
-# set this to 0 for Docker Hub
-create_ecr_repo=1
